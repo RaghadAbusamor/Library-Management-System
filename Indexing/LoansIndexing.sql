bd5df15f9ab35b369eadd652b07554e8dc11351b
Raghad Abusamor <raghadmohammad253@gmail.com>
2024-05-08 00:48:03 +0300

    Updating the tables with indexes

---
+++ Indexing/LoansIndexing.sql
@@ -0,0 +1,5 @@
+CREATE INDEX IX_loans_dateborrowed ON loans (dateborrowed);
+CREATE INDEX IX_loans_duedate ON loans (duedate);
+CREATE INDEX IX_loans_datereturned ON loans (datereturned);
+CREATE INDEX IX_loans_bookid ON loans (bookid);
+CREATE INDEX IX_loans_borrowerid ON loans (borrowerid);
